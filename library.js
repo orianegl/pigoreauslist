e0731e7796082792248325cc847740b6b08b6252
Jeremy Boggs <jeremy@virginia.edu>
2024-12-20 10:43:31 -0500

    Displays works list on the list page.

--- library.js
+++ library.js
@@ -12,27 +12,56 @@ async function parseCSV() {
 
     const rows = csvString.trim().split('\n'); // Split the CSV into rows
     const headers = rows[0].split(','); // Extract the header row
-    const data = [];
+    const rawData = [];
 
     for (let i = 1; i < rows.length; i++) { // Iterate through the rest of the rows
       const rowValues = rows[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
       const row = {};
 
       for (let j = 0; j < headers.length; j++) { // Create an object for each row
-        row[headers[j]] = rowValues[j];
+        row[headers[j]] = rowValues[j].replaceAll('"','');
       }
 
-      data.push(row);
+      rawData.push(row);
     }
 
     const listContainer = document.getElementById('list');
 
-    data.forEach(element => {
-        console.log(element);
-        const bookContainer = document.createElement('li');
-        bookContainer.textContent = element;
-        listContainer.appendChild(bookContainer);
+    // https://stackoverflow.com/questions/51009090/sort-and-group-objects-alphabetically-by-first-letter-javascript
+
+    let data = rawData.reduce((r, e) => {
+      // get first letter of name of current element
+      let group = e.TITLE[0];
+      // if there is no property in accumulator with this letter create it
+      if(!r[group]) r[group] = {group, children: [e]}
+      // if there is push current element to children array for that letter
+      else r[group].children.push(e);
+      // return accumulator
+      return r;
+    }, {});
+
+    //console.log(data);
+
+    Object.entries(data).forEach(([key, value]) => {
+
+      let letterContainer = document.createElement('li');
+      let worksList = document.createElement('ul');
+      letterContainer.textContent = key;
+      letterContainer.setAttribute('id', key);
+      letterContainer.appendChild(worksList);
+
+      let works = value.children;
+      works.forEach(function(work) {
+        let workListItem = document.createElement('li');
+        workListItem.textContent = work.TITLE;
+        worksList.appendChild(workListItem);
+      });
+
+      listContainer.appendChild(letterContainer);
+
+
     });
+
 }
 
 parseCSV();
